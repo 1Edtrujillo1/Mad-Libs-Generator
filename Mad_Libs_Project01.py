fd765c736f7bcdc27ef410bfb00b797dd24ddbaf
Eduardo Trujillo <act.jetv@gmail.com>
2021-06-14 21:09:57 -0500

    Adding button

--- Mad_Libs_Project01.py
+++ Mad_Libs_Project01.py
@@ -12,6 +12,7 @@ from tkinter import *
 root = Tk()
 root.geometry('300x300')
 root.title('ED Mad Libs Generator')
+
 Label(master=root, text='Mad Libs Generator \n Have Fun',
       font='arial 20 bold').pack()
 Label(master=root, text='Click Ant One :',
@@ -34,12 +35,41 @@ def madlib1():
     for i in range(0, len(vars_inputs)):
         dic[vars_inputs[i]] = input(inputs[i])
 
-    print('Say ' + list(dic)[0] + ', the photographer said as the camera flashed! ' +
-          list(dic)[1] + ' and I had gone to ' + list(dic)[2] + ' to get our photos taken on my birthday.' +
-          'The first photo we really wanted was a picture of us dressed as ' + list(dic)[3] +
-          ' pretending to be a ' + list(dic)[4] + '. When we saw the second photo,' +
+    dic = list(dic.values())
+
+    print('Say ' + dic[0] + ', the photographer said as the camera flashed! ' +
+          dic[1] + ' and I had gone to ' + dic[2] + ' to get our photos taken on my birthday.' +
+          'The first photo we really wanted was a picture of us dressed as ' + dic[3] +
+          ' pretending to be a ' + dic[4] + '. When we saw the second photo,' +
           'It was exactly what I wanted. We both looked like ' + list(dic)[5] + ' wearing ' +
-          list(dic)[6] + ' and ' + list(dic)[7] + ' --exactly what I had in mind.')
+          dic[6] + ' and ' + dic[7] + ' --exactly what I had in mind.')
+
+
+def madlib2():
+
+    vars_inputs = ["person", "color", "foods", "adjective", "thing",
+                   "place", "verb", "adverb", "food", "things"]
+
+    inputs = ["Enter a person name: ", "Enter a color name: ",
+              "Enter food name: ", "Enter an adjective name: ",
+              "Enter a thing name: ", "Enter a place name: ",
+              "Enter an verb name: ", "Enter an adverb name: ",
+              "Enter a food name: ", "Enter a thing name: "]
+
+    dic = {}
+    for i in range(0, len(vars_inputs)):
+        dic[vars_inputs[i]] = input(inputs[i])
+
+    dic = list(dic.values())
+
+    print("Today we picked apple from " + dic[0] + "´s Orchard. I had no idea there" +
+          " were so many different varieties of apples. I ate " + dic[1] + "apples straight" +
+          " off the tree that tested like " + dic[2] + ". Then there was a " +
+          dic[3] + " apple that looked like a " + dic[4] + ". When our bag were´" +
+          "full, we went on a free ride to " + dic[5] + " and back. It ended at" +
+          "a pile where we got to " + dic[6] + " " + dic[7] + ". I can hardly" +
+          "wait to get home and cook with the apples. We are going to make apple" +
+          dic[8] + " and " + dic[9] + " pies!")
 
 
 Button(root,
@@ -47,5 +77,11 @@ Button(root,
        font='arial 15',
        command=madlib1,
        bg='ghost white').place(x=60, y=120)
+
+Button(root,
+       text='The Butterfly',
+       font='arial 15',
+       command=madlib2,
+       bg='ghost white').place(x=70, y=180)
 
 root.mainloop()
